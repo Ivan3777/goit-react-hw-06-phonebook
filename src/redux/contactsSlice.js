6d2b188ded452a01611143794da057b755ceb0b3
Ivan Vodopianov <vis3010@gmail.com>
2022-12-14 18:34:49 +0200

    create redux components

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const initialState = {
+  contacts: [],
+  filter: '',
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
+      prepare(contactName, contactNumber) {
+        return {
+          payload: {
+            id: nanoid(),
+            name: contactName,
+            number: contactNumber,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.contacts.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.contacts.splice(index, 1);
+    },
+    setFilter(state, action) {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addContact, deleteContact, setFilter } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
